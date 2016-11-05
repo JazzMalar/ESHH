41a2e2ab0acaa874b0cc1b0c082415bfcff343df
Andreas <zuegerandreas@gmail.com>
2016-11-05 11:52:20 +0100

    LED's sollten nun angesprochen werden koennen

---
+++ source/01_Treiber/MainTest.py
@@ -0,0 +1,5 @@
+from c_LEDStrip2 import LEDStrip
+
+strip = LEDStrip(10,"WS2801",True)
+strip.getFullStrip()
+strip.setColorToStrip()
---
+++ source/01_Treiber/c_LEDStrip2.py
@@ -0,0 +1,104 @@
+# coding: utf8
+from c_LED2 import LED
+from c_logger import logger
+from c_LedHandler import LEDHandler
+import sys
+import socket
+import time
+
+
+
+
+class LEDStrip(object):
+
+    #WS2801
+    #LPD8806
+    #LPD6803
+    #SM16716
+    #debug = true --> test ohne RASPI, spi-funktionen werden ausgeklammert
+    def __init__(self,anzahlLEDs, stripType="WS2801",debug=False):
+        self.logger = logger("LEDStrip")
+        self.__leds     = anzahlLEDs
+        self.__strip = []
+        self.__setStripType(stripType)
+        self.__setDebug(debug)
+
+
+        self.logger.log("debug","create LEDStrip mit "+str(anzahlLEDs)+"LEDs")
+
+        for led in range(0, anzahlLEDs):
+            self.__strip.append(LEDHandler())
+        self.logger.log("debug","Strip:")
+        self.logger.log("debug",self.__strip)
+
+    def getFullStrip(self):
+        self.logger.log("debug","getFullStrip()")
+        return self.__strip
+
+    def getLEDNumber(self,ledNumber):
+        self.logger.log("debug","getLEDNumber ledNumber: "+str(ledNumber))
+        if ledNumber > (len(self.__strip) -1):
+            return -1
+        else:
+            return self.__strip[ledNumber]
+
+    def printStrip(self):
+        for i in range(0, len(self.__strip)-1):
+            led = self.getLEDNumber(i)
+            print "["+str(i)+"] RGB "+str(hex(led.getR()))+"/"+str(hex(led.getG()))+"/"+str(hex(led.getB()))+" Prio: "+str(led.getPrio())+" usedBy: "+str(led.getUsedBy())
+
+    def __setStripType(self,type):
+        self.stripType = type
+
+    def __getStripType(self):
+        return self.stripType
+
+    def __setDebug(self,onOff):
+        self.debug= onOff
+
+    def __getDebug(self):
+        return self.debug
+
+    def setColorToStrip(self):
+        self.__write_stream(self.__strip)
+
+    def __write_stream(self,pixels):
+        # 3 bytes per pixel
+        PIXEL_SIZE = 3
+        PIXEL_SIZE_SM16716 = 4
+
+        if self.__getStripType() == "LPD6803":
+            pixel_out_bytes = bytearray(2)
+            if not self.__getDebug():
+                spidev.write(bytearray(4))
+            pixel_count = len(pixels) / PIXEL_SIZE
+            for pixel_index in range(pixel_count):
+                pixel_in = bytearray(pixels[(pixel_index * PIXEL_SIZE):((pixel_index * PIXEL_SIZE) + PIXEL_SIZE)])
+
+                pixel_out = 0b1000000000000000  # bit 16 must be ON
+                pixel_out |= (pixel_in[0] & 0x00F8) << 7  # RED is bits 11-15
+                pixel_out |= (pixel_in[1] & 0x00F8) << 2  # GREEN is bits 6-10
+                pixel_out |= (pixel_in[2] & 0x00F8) >> 3  # BLUE is bits 1-5
+
+                pixel_out_bytes[0] = (pixel_out & 0xFF00) >> 8
+                pixel_out_bytes[1] = (pixel_out & 0x00FF) >> 0
+                if not self.__getDebug():
+                    spidev.write(pixel_out_bytes)
+            if not self.__getDebug():
+                spidev.write(bytearray(len(pixels) / 8 + 1))
+        elif self.__getStripType() == "LPD8806":
+            if not self.__getDebug():
+                spidev.write(pixels)
+                spidev.write(bytearray(b'\x00\x00\x00'))  # zero fill the last to prevent stray colors at the end
+                spidev.write(bytearray(b'\x00'))
+        elif self.__getStripType() == "SM16716":
+            # Each frame for SM17616 starts with 50bits set to '0'
+            # Also every pixel needs to start with a bit set to '1'
+            if not self.__getDebug():
+                spidev.write(bytearray(
+                b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00') + pixels)
+        else:
+            if not self.__getDebug():
+                spidev.write(pixels)
+
+        return
