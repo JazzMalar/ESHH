d3b5ab0d4d9b7747245a4fdfa4635ad0945edbb2
Andreas <zuegerandreas@gmail.com>
2017-01-09 20:46:29 +0100

    add print

--- source/01_Treiber/c_LEDStrip2.py
+++ source/01_Treiber/c_LEDStrip2.py
@@ -111,6 +111,7 @@ class LEDStrip(object):
 
     def __writeStream(self, pixels):
         # 3 bytes per pixel
+        print "__writeStream getDebug:"+str(self.__getDebug())
         PIXEL_SIZE = 3
         PIXEL_SIZE_SM16716 = 4
         if not self.__getDebug():
